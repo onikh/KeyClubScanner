85598394ca00461dff900abb7a778396fcfad72e
onikh <onikh738@gmail.com>
2023-03-04 17:37:32 -0600

    finished prototype

--- Student.py
+++ Student.py
@@ -1,12 +1,14 @@
 import keyboard
 from pyzbar import pyzbar
+import ast
 
 class Student: 
-    attendedMeetings = {}
+    
 
-    def __init__(self, firstName, lastName, studentID, email):
+    def __init__(self, studentID, lastName, firstName, email, attendedMeetings):
         self.studentID = studentID
-        self.firstName = firstName
         self.lastName = lastName
+        self.firstName = firstName
         self.email = email
+        self.attendedMeetings = ast.literal_eval(attendedMeetings)
         
--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-import Student
+from Student import Student
+import csv
 
 students = []
 
-with open("test.csv") as file:
-  for item in file:
-    students.append(item.rstrip("\n"))
+with open("Attendance.csv") as file:
+  reader = csv.reader(file)
+  for row in reader:
+    students.append(Student(row[0], row[1], row[2], row[3], row[4]))
 
-print(students)
+for student in students:
+  print(student.attendedMeetings)
 
+
+while True: 
+  scan = input("Scan your barcode.")
+
+  if scan == "exit": 
+    break
+
+  for student in students:
+    if scan == student.studentID:
+      student.attendedMeetings["Flag Routes"] = 2
+      print("")
+      print(student.firstName + "'s attendance record: ")
+      print(student.attendedMeetings)
+      print("")
+      break
+
+
+
+
+f = open("Attendance.csv", "w")
+
+for student in students:
+  row = student.studentID + "," + student.lastName + "," + student.firstName + "," + student.email + "," + str(student.attendedMeetings) + "\n"
+  f.write(row)
+ 
+f.close()
+
+
+
+    
 
     
